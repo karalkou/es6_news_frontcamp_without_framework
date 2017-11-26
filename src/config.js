81c830ff43ae31461c2d5c599b419d13a35e3066
Karalkou <yury.karalkou@gmail.com>
2017-11-26 23:31:16 +0300

    Add webpack

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const apiKey = '90e3266a077347219e896b2dec2ec141';
+export const defaultSource = 'bbc-news';
+export const customImageUrl = `http://bydesignvsm.com/wp-content/uploads/2015/06/Black-Background-HD-Wallpaper-24.jpg`;
