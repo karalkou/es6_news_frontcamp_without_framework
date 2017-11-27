7788d899ce544c5ffcdddfdf50acedb66b6997e4
Karalkou <yury.karalkou@gmail.com>
2017-11-27 07:18:05 +0300

    Rearrange styles and js. Assemble release folder

--- src/config.js
+++ src/config.js
@@ -1,3 +1,14 @@
 export const apiKey = '90e3266a077347219e896b2dec2ec141';
+
 export const defaultSource = 'bbc-news';
+
 export const customImageUrl = 'http://bydesignvsm.com/wp-content/uploads/2015/06/Black-Background-HD-Wallpaper-24.jpg';
+
+export const dateOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric'
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import {apiKey} from './config';
-import {defaultSource} from './config';
-import {customImageUrl} from './config'
+import {apiKey, defaultSource, customImageUrl, dateOptions} from './config';
 
 import './css/styles.css';
 
@@ -11,6 +9,11 @@ export default class NewsList{
         this.SOURCE = null;
     }
 
+    /**
+     * Inits instance
+     * @param node {DOM node} - node to apply class
+     * @param initialSource {String} - initial news source
+     */
     init(node, initialSource){
         this.NEWS_CONTAINER = node;
         this.NEWS_SOURCE_TITLE = document.querySelector('.news-source');
@@ -21,30 +24,42 @@ export default class NewsList{
         document.addEventListener('click', this.clickHandler);
     }
 
+    /**
+     * Renders result to DOM
+     * @param stringNodes {String} - assembled news items
+     * @param source {String} - news source
+     */
     render({nodes: stringNodes, source}) {
         this.NEWS_SOURCE_TITLE.innerHTML = source;
         this.NEWS_CONTAINER.innerHTML = '';
         stringNodes.map((el) => this.NEWS_CONTAINER.insertAdjacentHTML('afterbegin', el));
     };
 
+    /**
+     * Parses data from response
+     * @param data {Object} - data from API
+     * @returns {Array|*|{}}
+     */
     parseData(data) {
         return data.articles.map( (item) => {
-            const customDate = new Date().toISOString().slice(0, 10).toString();
+            const { urlToImage, publishedAt, author, title, description, url } = item;
+            const formattedPublishedAt = new Date(publishedAt).toLocaleString("en", dateOptions);
+
             const newsCard = `
-                <div class="news-card">
-                    <img class="news-card__img" src="${item.urlToImage || customImageUrl}" alt="News Article Image">
-                    <div class="news-card__wrapper">
-                        <div class="news-card__header">
-                            <div class="news-card__date">
-                                
+                <div class="news-list-item">
+                    <img class="news-list-item__img" src="${urlToImage || customImageUrl}" alt="News Article Image">
+                    <div class="news-list-item__wrapper">
+                        <div class="news-list-item__header">
+                            <div class="news-list-item__date">
+                                ${formattedPublishedAt}
                             </div>
                         </div>
-                        <div class="news-card__data">
-                            <div class="news-card__content">
-                                <span class="news-card__author">${item.author}</span>
-                                <h1 class="news-card__title"><a href="#">${item.title}</a></h1>
-                                <p class="news-card__description">${item.description}</p>
-                                <a href="${item.url}" class="news-card__button">Read more</a>
+                        <div class="news-list-item__data">
+                            <div class="news-list-item__content">
+                                <span class="news-list-item__author">${author}</span>
+                                <h1 class="news-list-item__title"><a href="#">${title}</a></h1>
+                                <p class="news-list-item__description">${description}</p>
+                                <a href="${url}" class="news-list-item__button">Read more</a>
                             </div>
                         </div>
                     </div>
@@ -54,10 +69,19 @@ export default class NewsList{
         });
     }
 
+    /**
+     * Parses JSON
+     * @param data {Promise} - data from response
+     * @returns {*} JSON object
+     */
     parseJSON(data){
         return data.json();
     };
 
+    /**
+     * Sends fetch request
+     * @param source {String} - news source
+     */
     triggerFetch(source = defaultSource) {
         let that = this;
         fetch(this.buildUrl(source))
@@ -67,12 +91,21 @@ export default class NewsList{
             .catch((err) => console.log(err));
     };
 
+    /**
+     * Builds url to make fetch request
+     * @param source {String} - news source
+     * @returns {string} - url
+     */
     buildUrl(source){
         if ( source && source.length > 0 ) {
             return `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`
         }
     };
 
+    /**
+     * Handles click on document
+     * @param e - event
+     */
     clickHandler = (e) => {
         let target = e.target;
         if (target.classList.contains('source-list__item')) {
@@ -83,6 +116,7 @@ export default class NewsList{
 };
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+
 document.addEventListener('DOMContentLoaded', () => {
     const newsContainer = document.querySelector('.news-container');
     if ( newsContainer ) new NewsList().init(newsContainer, defaultSource);
