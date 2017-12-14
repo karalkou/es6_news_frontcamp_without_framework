27e493d8af063e4494b625b1cc3b08a8caec174b
Karalkou <yury.karalkou@gmail.com>
2017-12-14 22:54:21 +0300

    [reconfiguration] Start to reconfigure webpack using webpack-merge

--- webpack-configs/webpack.production.config.js
+++ webpack-configs/webpack.production.config.js
@@ -1,65 +1,66 @@
-import webpack from 'webpack';
-import Config from 'webpack-config';
+const webpack = require('webpack');
 
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-export default new Config().extend('webpack-configs/webpack.base.config.js').merge({
-    devtool: 'source-map',
+module.exports = function () {
+    return {
+        devtool: 'source-map',
 
-    module: {
-        rules: [
-            /* js */
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: "babel-loader" },
-                    /*{ loader: 'eslint-loader' }*/
-                ]
-            },
-            /* less */
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
+        module: {
+            rules: [
+                /* js */
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
                     use: [
-                        { loader: 'css-loader', options: { sourceMap: true, minimize: true, importLoaders: 1 } },
-                        { loader: "postcss-loader", options: { sourceMap: true } },
-                        { loader: 'less-loader', options: { sourceMap: true } }
+                        { loader: "babel-loader" },
+                        /*{ loader: 'eslint-loader' }*/
                     ]
-                })
-            },
-            /* css */
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        { loader: 'css-loader', options: { sourceMap: false, minimize: true, importLoaders: 1 } },
-                        { loader: "postcss-loader", options: { sourceMap: true } }
-                    ]
-                })
-            }
+                },
+                /* less */
+                {
+                    test: /\.less$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            { loader: 'css-loader', options: { sourceMap: true, minimize: true, importLoaders: 1 } },
+                            { loader: "postcss-loader", options: { sourceMap: true } },
+                            { loader: 'less-loader', options: { sourceMap: true } }
+                        ]
+                    })
+                },
+                /* css */
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            { loader: 'css-loader', options: { sourceMap: false, minimize: true, importLoaders: 1 } },
+                            { loader: "postcss-loader", options: { sourceMap: true } }
+                        ]
+                    })
+                }
+            ]
+        },
+
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                sourceMap: true,
+                compress: {
+                    warnings: true
+                }
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common',
+                filename: 'js/common.js',
+                minChunks: 2
+            }),
+            new ExtractTextPlugin({
+                filename: "css/[name].css"
+            }),
+            /*new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            })*/
         ]
-    },
-
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: true
-            }
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            filename: 'js/common.js',
-            minChunks: 2
-        }),
-        new ExtractTextPlugin({
-            filename: "css/[name].css"
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        })
-    ]
-});
+    }
+};
