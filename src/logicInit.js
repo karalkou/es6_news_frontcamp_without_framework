dea3b389118436073c9c406abbcefd312fa816e1
Yury Karalkou <yury.karalkou@gmail.com>
2018-01-05 19:52:15 +0300

    [HT4_Design_Patterns] Implement the Prototype design pattern

--- src/logicInit.js
+++ src/logicInit.js
@@ -5,11 +5,31 @@ import SourcesTitle from './components/SourcesTitle';
 import './styles/dynamic-styles.less';
 
 export default () => {
-    const newsSourceControls = document.querySelector('.source-list');
+    const newsSourceList = document.querySelector('.source-list');
     const newsSourceTitle = document.querySelector('.news-source');
     const newsContainer = document.querySelector('.news-container');
 
-    if ( newsSourceControls ) { new SourcesList(newsSourceControls).init(); }
-    if ( newsSourceTitle ) { new SourcesTitle(newsSourceTitle).init(); }
-    if ( newsContainer ) { new NewsList(newsContainer).init(); }
+    // Object creation in beneath strings shows us an implementation of the Prototype Design Pattern
+    // In this special occasion it is an unnecessary overhead
+    // Pattern realisation was borrowed from
+    // https://github.com/fbeline/Design-Patterns-JS/blob/master/src/creational/prototype/prototype_es6.js
+    if ( newsSourceList ) {
+        let sourcesListInstance = new SourcesList(newsSourceList);
+        sourcesListInstance.clone().init();
+    }
+    if ( newsSourceTitle ) {
+        let SourcesTitleInstance = new SourcesTitle(newsSourceTitle);
+        SourcesTitleInstance.clone().init();
+    }
+    if ( newsContainer ) {
+        let NewsListInstance = new NewsList(newsContainer);
+        NewsListInstance.clone().init();
+    }
+
+
+
+    // The 'old' code (before the Prototype pattern implementation)
+    // if ( newsSourceList ) { new SourcesList(newsSourceList).init(); }
+    // if ( newsSourceTitle ) { new SourcesTitle(newsSourceTitle).init(); }
+    // if ( newsContainer ) { new NewsList(newsContainer).init(); }
 }
