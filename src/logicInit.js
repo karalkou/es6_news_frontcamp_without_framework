2cb495469ae4ee08cb3a1f5aedc9a9d88036c65d
Yury Karalkou <yury.karalkou@gmail.com>
2018-01-10 19:45:06 +0300

    [HT4_Design_Patterns] Replace class Facade with createAndInit facade function

--- src/logicInit.js
+++ src/logicInit.js
@@ -5,28 +5,26 @@ import SourcesTitle from './components/SourcesTitle';
 import './styles/dynamic-styles.less';
 
 // I suppose it can be treated as Facade pattern, cause we simplify initializing for users
-class Facade{
-    constructor(node, classToCreate){
-        // Prototype Design Pattern
-        // Object creation below shows us an implementation of the Prototype Design Pattern
-        // In this special occasion it is an unnecessary overhead
-        // Pattern realisation was borrowed from
-        // https://github.com/fbeline/Design-Patterns-JS/blob/master/src/creational/prototype/prototype_es6.js
-        if( node ){
-            let instance = new classToCreate(node);
-            instance.clone().init();
-        }
+const createAndInit = (node, classToCreate) => {
+    // Prototype Design Pattern
+    // Object creation below shows us an implementation of the Prototype Design Pattern
+    // In this special occasion it is an unnecessary overhead
+    // Pattern realisation was borrowed from
+    // https://github.com/fbeline/Design-Patterns-JS/blob/master/src/creational/prototype/prototype_es6.js
+    if( node ){
+        let instance = new classToCreate(node);
+        instance.clone().init();
     }
-}
+};
 
 export default () => {
     const newsSourceList = document.querySelector('.source-list');
     const newsSourceTitle = document.querySelector('.news-source');
     const newsContainer = document.querySelector('.news-container');
 
-    new Facade(newsSourceList, SourcesList);
-    new Facade(newsSourceTitle, SourcesTitle);
-    new Facade(newsContainer, NewsList);
+    createAndInit(newsSourceList, SourcesList);
+    createAndInit(newsSourceTitle, SourcesTitle);
+    createAndInit(newsContainer, NewsList);
 
 
     // The 'old' code (before prototype and facade patterns implementation)
